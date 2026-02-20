7cd373ea02b7c97a7f3d67dee1359cb81b855e81
RhettiBOi <vdhrhett@icloud.com>
2026-02-20 05:41:01 +0200

    Contoller component

---
+++ ClassProjects/Assets/Scripts/Player1.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player1 : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float Movespeed = 5f;
+    [SerializeField] private float Jumpstep = 1f;
+    [SerializeField] private float Rotation = 100f;
+    private Vector2 lookinput;
+    private Vector2 moveinput;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yaw = lookinput.x * Rotation * Time.deltaTime;
+        transform.Rotate(0f, yaw, 0f, Space.World);
+
+        Vector3 move3 = new Vector3(moveinput.x, 0f, moveinput.y)*Movespeed;
+        transform.position += move3;
+    }
+    
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        moveinput = context.ReadValue<Vector2>();
+    }
+
+    public void Onlook(InputAction.CallbackContext context)
+    {
+        lookinput = context.ReadValue<Vector2>();
+    }
+
+    public void onjump(InputAction.CallbackContext context) 
+    {
+        if (!context.performed) return;
+        transform.position += Vector3.up * Jumpstep;
+    }
+}
+
+ 
--- ClassProjects/Assets/Scripts/TPOKB.cs
+++ ClassProjects/Assets/Scripts/TPOKB.cs
@@ -5,30 +5,67 @@ public class TPOKB : MonoBehaviour
 {
     [SerializeField] private InputActionReference P1Move;
     [SerializeField] private InputActionReference P2Move;
+    [SerializeField] private InputActionReference P1Jumping;
+    [SerializeField] private InputActionReference P2Jumping;
 
-    [SerializeField] private Transform P1;
-    [SerializeField] private Transform P2;
+    [SerializeField] private GameObject P1;
+    [SerializeField] private GameObject P2;
 
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float JumpForce = 100f;
+
+    /*public Rigidbody rb1;
+    public Rigidbody rb2;*/
 
     private void OnEnable()
     {
         P1Move.action.Enable();
         P2Move.action.Enable();
+        P1Jumping.action.Enable();
+        P2Jumping.action.Enable();
+
+       //P1Jumping.action.performed += ctx => P1Jump();
+       //P2Jumping.action.performed += ctx => P2Jump();
     }
 
     private void OnDisable()
     {
         P1Move.action.Disable();
         P2Move.action.Disable();
+        P1Jumping.action.Disable();
+        P2Jumping.action.Disable();
     }
 
     private void Update()
     {
+        //CharacterController controller = GetComponent<CharacterController>();
+
         var m1 = P1Move.action.ReadValue<Vector2>();
         var m2 = P2Move.action.ReadValue<Vector2>();
 
-        if (P1) P1.position += new Vector3(m1.x, 0f, m1.y) * speed * Time.deltaTime;
-        if (P2) P2.position += new Vector3(m2.x, 0f, m2.y) * speed * Time.deltaTime;
+        if (P1) P1.transform.position += new Vector3(m1.x, 0f, m1.y) * speed * Time.deltaTime;
+        if (P2) P2.transform.position += new Vector3(m2.x, 0f, m2.y) * speed * Time.deltaTime;
+
+        if (P1Jumping.action.triggered/* && controller.isGrounded*/)
+        {
+            P1.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+
+        if (P2Jumping.action.triggered)
+        {
+            P2.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+
     }
+
+   /* void P1Jump()
+    {
+        P1.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpForce);
+        Debug.Log("Jump Pressed");
+    }
+
+    void P2Jump()
+    {
+        P2.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpForce);
+    }*/
 }
